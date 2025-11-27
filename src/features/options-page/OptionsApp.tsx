4450bc408f0bd2b0bec24661b1d137f73efddce6
youling <liyoujun.lyj@digital-engine.com>
2025-11-27 15:53:28 +0800

    chore: release v1.9.1

--- src/features/options-page/OptionsApp.tsx
+++ src/features/options-page/OptionsApp.tsx
@@ -262,7 +262,7 @@ export const OptionsApp: React.FC = () => {
           </PageDescription>
         </HeaderContent>
         <HeaderActions>
-          <VersionTag>v1.9.0</VersionTag>
+          <VersionTag>v1.9.1</VersionTag>
           <Button onClick={openReleasePage}>
             检查更新
           </Button>
